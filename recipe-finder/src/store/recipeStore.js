872622e6eeaf7f94e3bbff6edf2d6dbd05ffad82
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-12-19 13:41:02 +0100

    add Zustand store for recipes, query, loading, error, and selected recipe

---
+++ recipe-finder/src/store/recipeStore.js
@@ -0,0 +1,42 @@
+import { create } from "zustand";
+import { searchMealsByName, searchMealByID, searchMealByIngredients } from "../services/mealApi";
+
+const useRecipeStore = create ((set) => ({
+    recipes: [],
+    query: "",
+    loading : false,
+    error: null,
+    selectedRecipe: null,
+   setQuery: (q) => set({ query: q}),
+   
+   searchByName: async (name) => {
+    set({ loading: true, error: null});
+    try {
+        const data = await searchMealsByName(name);
+        set({recipes: data || [], loading:false});
+    } catch (error){
+        set({ error: error.message, loading:false});
+    }
+   },
+
+   searchByIngredients: async (ingredient) =>{
+    set({loading: true, error: null});
+    try{
+        const data = await searchMealByIngredients(ingredient);
+        set ({ recipes: data || [], loading: false});
+    } catch (error){
+        set ({ error: error.message, loading: false})
+    }
+   },
+
+   searchByID: async (id) => {
+    set ({loading: true, error: null});
+    try {
+        const data = await searchMealByID(id);
+        set ({ selectedRecipe : data, loading: false})
+    } catch (error) {
+    set ({ error: error.message, loading: false})
+   }
+}}));
+
+export default useRecipeStore;

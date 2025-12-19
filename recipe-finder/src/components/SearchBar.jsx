c0e98ced7c77a268770f7ce7e750a276f56338c7
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-12-19 14:09:44 +0100

    create SearchBar component connected to Zustand store

---
+++ recipe-finder/src/components/SearchBar.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useRecipeStore } from "../store/recipeStore";
+
+const searchBar = () =>{
+    const [input, setInput] = useState("");
+    const searchByName = useRecipeStore((state) => state.searchByName);
+    const searchByIngredient = useRecipeStore((state) => state.searchByIngredient);
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        if (input.trim() === "") return;
+    
+    searchByName(input.trim());
+    setInput("");
+    }
+
+    return (
+        <form
+        onSubmit={handlesubmit}
+        className="flex justify-center items-center my-6">
+            <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+             className="p-3 rounded-l-lg w-80 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+      />
+      <button
+      type="submit"
+      className="bg-green-500 text-white px-4 py-3 rounded-r-lg hover:bg-green-600">
+        Search
+      </button>
+    </form>
+    );
+};
+
+export default searchBar;
